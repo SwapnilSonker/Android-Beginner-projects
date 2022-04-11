eb97129c169ac478d29380d62d05fec5f12fd141
Swapnil Sonker <90846252+SwapnilSonker@users.noreply.github.com>
2022-04-11 22:42:13 +0530

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,11 +1,35 @@
-package com.example.happybirthday
+package com.example.demoproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.demoproject.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{ calculateTip() }
+    }
+    private fun calculateTip()
+    {
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOption.checkedRadioButtonId
+
+        val tipPercentage = when(selectedId){
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if(roundUp){
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount , formattedTip)
     }
 }
